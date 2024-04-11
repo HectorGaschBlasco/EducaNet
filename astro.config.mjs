b8f0fdda9993755e731f0cc61c651e3978dbb70b
hectorgaschblasco <hector.gasch.blasco@gmail.com>
2024-04-12 00:26:30 +0200

    Sign In design complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import db from "@astrojs/db";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db(), tailwind()],
+  devToolbar: {
+    enabled: false
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -3,17 +3,17 @@ import { db, Teacher, Student_Teacher, Student, Class_Student, Class, Department
 // https://astro.build/db/seed
 export default async function seed() {
 	
-	await db.insert(Department).values([
-	{ id: "1" , location: "Computing Science"},
-	])
+	// await db.insert(Department).values([
+	// { id: "1" , location: "Computing Science"},
+	// ])
 
-	await db.insert(Teacher).values([
-		{ id: "1" , dni: "20955322E", name: "Profesor A",email: "profesora@mail.com", password: "password", departmentId: "1"},
-	])
+	// await db.insert(Teacher).values([
+	// 	{ id: "1" , dni: "20955322E", name: "Profesor A",email: "profesora@mail.com", password: "password", departmentId: "1"},
+	// ])
 
-	await db.insert(Student).values([
-		{ id: "1" , nia: "10663305", dni: "20955322E", name: "Profesor A",email: "profesora@mail.com", password: "password"},
-	])
+	// await db.insert(Student).values([
+	// 	{ id: "1" , nia: "10663305", dni: "20955322E", name: "Profesor A",email: "profesora@mail.com", password: "password"},
+	// ])
 
 	// await db.insert(Student_Teacher).values([
 	// 	{ studentId: "1" , teacherId: "1"},
