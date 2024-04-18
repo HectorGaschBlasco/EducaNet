f259e0cc44b561c0013fe9aaaf08951293ff6bf8
HectorGaschBlasco <145042065+HectorGaschBlasco@users.noreply.github.com>
2024-04-18 11:44:43 +0200

    Update checkPassword.ts

--- src/utils/checkPassword.ts
+++ src/utils/checkPassword.ts
@@ -1,16 +1,16 @@
 import bcrypt from 'bcrypt'
 
-export async function checkPassword ( storedPassword : string , providedPassword : string ) : Promise< boolean > {
+export async function checkPassword ( storedPassword : string , providedPassword : string ) : Promise < boolean > {
     
-    try{
+    try {
 
         return await bcrypt.compare(providedPassword, storedPassword);
 
-    }catch(e){
+    } catch( e ) {
 
         console.log('Error comparing passwords', e);
 
         throw e;
-
+        
     }
 }
