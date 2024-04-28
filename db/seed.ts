055faea5049819769703925302ed76c018f70d4f
hectorgaschblasco <hector.gasch.blasco@gmail.com>
2024-04-29 00:31:25 +0200

    Home created

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,4 @@
-// import { db, Teacher, Student_Teacher, Student, Class_Student, Class, Department, Subject } from "astro:db";
+import { db, Teacher, Student_Teacher, Student, Class_Student, Class, Department, Subject } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -12,16 +12,13 @@ export default async function seed() {
 	// ])
 
 	// await db.insert(Student).values([
-	// 	{ id: "1" , nia: "10663305", dni: "20955322E", name: "Profesor A",email: "profesora@mail.com", password: "password"},
+	// 	{ id: "1" , nia: "10663305", dni: "20955322E", name: "Profesor A",email: "a@a.a", password: "a"},
 	// ])
 
 	// await db.insert(Student_Teacher).values([
 	// 	{ studentId: "1" , teacherId: "1"},
 	// ])
 
-	// await db.insert(Class_Student).values([
-	// 	{ classId: "1" , studentId: "1"},
-	// ])
 
 	// await db.insert(Class).values([
 	// 	{ id: "1" , description: "Computing Science", teacherId:"1"},
@@ -30,4 +27,8 @@ export default async function seed() {
 	// await db.insert(Subject).values([
 	// 	{ id: "1" , description: "Computing Science", classId: "1"},
 	// ])
+	// await db.insert(Class_Student).values([
+	// 	{ classId: "1" , studentId: "1"},
+	// ])
+
 }
