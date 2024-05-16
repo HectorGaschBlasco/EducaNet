f40010ab35114721508f4454d8cefa19d4762429
hectorgaschblasco <hector.gasch.blasco@gmail.com>
2024-05-17 00:16:23 +0200

    Endpoint completed

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,17 +1,27 @@
 import type { APIRoute } from "astro";
 
+var clicked_class = null
+var json_class = JSON.stringify({})
+
 export const POST: APIRoute = async ({ request }) => {
   
     if (request.headers.get("Content-Type") === "application/json") {
-      const body = await request.json();
-      const nombre = body.title;
-      console.log(nombre)
-      return new Response(JSON.stringify({
-        title: nombre
-      }), {
-        status: 200
+      clicked_class = await request.json();
+      const name = clicked_class.title;
+      json_class = JSON.stringify({
+        "title": name
       })
-    }
-    return new Response(null, { status: 400 });
-  };
+      return new Response(null,{ status: 200 } )
+    } 
+  return new Response(null, { status: 400 })
+}
+
+
+export const GET: APIRoute = async ({ request }) => {
+  
+  if (request.headers.get("Content-Type") === "application/json") {
+    return new Response(json_class,{ status: 200 })
+  }
+return new Response(null, { status: 400 })
+}
   
